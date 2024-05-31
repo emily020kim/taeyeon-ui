b3f32131beeeb393fe843e096dac22d38584ecb4
emily020kim <emily020kim@gmail.com>
2024-05-31 12:29:05 -0700

    working on albums

--- src/pages/Albums.tsx
+++ src/pages/Albums.tsx
@@ -1,7 +1,24 @@
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import { useEffect, useState } from "react";
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import 'react-vertical-timeline-component/style.min.css';
 
 const Albums = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://blooming-sands-12721-c98e11415d2d.herokuapp.com/albums');
+        const parsedData = await response.json();
+        setData(parsedData);
+      } catch(error) {
+        console.error('Trouble fetching from albums endpoint: ', error);
+      }
+    }
+
+    fetchData();
+  }, [])
+
   return (
     <section className="max-container">
         <h1 className="text-4xl font-semibold yellow-gradient_text uppercase">
@@ -11,6 +28,16 @@ const Albums = () => {
         <p className="text-lg font-normal text-[#ffecb4] uppercase my-8">
             taeyeon's entire discography to date.
         </p>
+
+        <div className='mt-12 flex'>
+          <VerticalTimeline>
+            {data.map((album, index) => {
+              <VerticalTimelineElement>
+                
+              </VerticalTimelineElement>
+            })}
+          </VerticalTimeline>
+        </div>
     </section>
   )
 }
